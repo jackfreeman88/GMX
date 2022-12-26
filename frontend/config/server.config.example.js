e1e4442e6485bb322f3e314d9afba1fda71664e8
Lalit Bhanderi <lalit.nyusoft@gmail.com>
2022-12-26 11:46:44 +0530

    Added on server.config.example in the config directory

---
+++ frontend/config/server.config.example.js
@@ -0,0 +1,6 @@
+export const REACT_APP_API_BASE_URL = "http://localhost:3006/api/v1";
+export const APP_URL = "http://localhost:3000/";
+export const ASSETS_URL = "http://localhost/gmx/assets";
+export const APP_NAME = "GMX";
+export const SOCKET_URL = "http://localhost:3001/";
+export const RECAPTCHA_SITE_KEY = "6LfphBgjAAAAAFSzjfFDQ6LW_PCuo9Qo3m0x71-H";
